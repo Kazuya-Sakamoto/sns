c96f53ac7da3aa37a1f2fa644cee42cc22da18f9
Kazuya Sakamoto <56709557+Kazuya-Sakamoto@users.noreply.github.com>
2020-05-26 15:41:09 +0900

    create PostList view

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={ styles.container }>
+      <View style={ styles.postWrapper }>
+        <View style={ styles.topBox }>
+          <View style={ styles.topBoxLeft }></View>
+        </View>
+        <View style={ styles.middleBox }>
+          <Image
+          style={ styles.middleBox }
+          source={{
+            uri: 'https://picsum.photos/200/300',
+          }}
+          />
+        </View>
+        <View style={ styles.bottomBox }>
+          <View style={ styles.bottomBoxArea }>
+            <View style={ styles.bottomLeftArea }></View>
+            <View style={ styles.bottomCenterArea }></View>
+            <View style={ styles.bottomRightArea }></View>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
@@ -16,4 +35,50 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  postWrapper: {
+    height: 500,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  topBox: {
+    width: '100%',
+    height: 54,
+    backgroundColor: 'red',
+  },
+  topBoxLeft: {
+    width: 300,
+    height: 54,
+    backgroundColor: 'white'
+  },
+  middleBox: {
+    width: '100%',
+    height: 350,
+  },
+  bottomBox: {
+    width: '100%',
+    height: 140,
+    backgroundColor: 'yellow',
+  },
+  bottomBoxArea: {
+    width: '100%',
+    height: 50,
+    backgroundColor: 'gray',
+    flexDirection: 'row'
+  },
+  bottomLeftArea: {
+    width: 150,
+    height: 50,
+    backgroundColor: 'white'
+  },
+  bottomCenterArea: {
+    width: 100,
+    height: 50,
+    backgroundColor: 'blue'
+  },
+  bottomRightArea: {
+    width: 125,
+    height: 50,
+    backgroundColor: 'green'
+  }
 });

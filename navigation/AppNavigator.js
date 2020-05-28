3951334ad181d58b18e093c5ad0848b6f174e0b8
Kazuya Sakamoto <56709557+Kazuya-Sakamoto@users.noreply.github.com>
2020-05-28 18:53:56 +0900

    create icon// font

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -4,17 +4,95 @@ import ListItem from '../components/ListItem'
 import posts from '../dummies/posts.json'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from "../screens/HomeScreen"
+import HomeScreen from "../screens/HomeScreen";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import LikeScreen from '../screens/LikeScreen';
+import SearchScreen from '../screens/SearchScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import PostScreen from '../screens/PostScreen';
+import { FontAwesome } from '@expo/vector-icons';
+
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
+// * 一覧画面
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Instagram" component={ HomeScreen } />
+    </Stack.Navigator>
+  )
+}
+
+// * 検索画面 
+const SearchStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="検索" component={ SearchScreen }/>
+    </Stack.Navigator>
+  )
+}
+
+// * 新規投稿画面 
+const PostStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="投稿" component={ PostScreen }/>
+    </Stack.Navigator>
+  )
+}
+
+// * Follow いいね 画面
+const LikeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="アクティビティ" component={ LikeScreen }/>
+    </Stack.Navigator>
+  )
+}
+
+// * プロフィール画面
+const ProfileStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="プロフィール" component={ ProfileScreen }/>
+    </Stack.Navigator>
+  )
+} 
+
+// * FontAwesome Icon導入
+const screenOption = ({ route }) => ({
+  tabBarIcon: ({ color, size }) => {
+    let iconName;
+
+    if (route.name === '     ') {
+      iconName = "home";
+    } else if (route.name === '  ') {
+      iconName = "heart-o";
+    } else if (route.name === '    ') {
+      iconName = "search";
+    } else if (route.name === '   ') {
+      iconName = "plus-square-o";
+    } else if (route.name === ' ') {
+      iconName = "photo";
+    }
+
+    // You can return any component that you like here!
+    return <FontAwesome name={iconName} size={25} color={color} />;
+  },
+})
 
 export default AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Instagram" component={HomeScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator screenOptions={ screenOption }>
+      <Tab.Screen name="     " component={ HomeStack } />
+      <Tab.Screen name="    " component={ SearchStack } />
+      <Tab.Screen name="   " component={ PostStack } />
+      <Tab.Screen name="  " component={ LikeStack } />
+      <Tab.Screen name=" " component={ ProfileStack } />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
